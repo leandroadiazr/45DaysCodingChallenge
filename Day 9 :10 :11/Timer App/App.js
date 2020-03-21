8bb7e3d83768e5b63be13d2205a508c28f2fdbe8
leandroadiazr <leandroadiazr@gmail.com>
2020-03-21 18:04:20 -0400

    Day 9 10 11

---
+++ Day 9 :10 :11/Timer App/App.js
@@ -0,0 +1,47 @@
+console.log("TimerApp Connected");
+
+//TIMER APP
+
+//Main Goal implementation
+//select dom elements
+const initialTime = document.querySelector(".initialTime");
+const start = document.querySelector(".start");
+const stop = document.querySelector(".stop");
+const reset = document.querySelector(".reset");
+const circle = document.querySelector("circle");
+const display = document.querySelector("#timer");
+
+// circle function
+let duration;
+const perimeter = circle.getAttribute("r") * 2 * Math.PI;
+circle.setAttribute("stroke-dasharray", perimeter);
+
+// 1.2 Creating new class with my html inputs
+const timer = new Timer(initialTime, start, stop, reset, {
+	onStart(totalDuration) {
+		duration = totalDuration;
+		console.log("OnStart");
+	},
+	onTick(timeRemaining) {
+		console.log("ticking");
+		display.textContent = timeRemaining.toFixed(0);
+		circle.setAttribute(
+			"stroke-dashoffset",
+			(perimeter * timeRemaining) / duration - perimeter
+		);
+	},
+	onCompleted() {
+		console.log("completed");
+		timeRemaining = 0;
+	}
+});
+
+//2.- Draw full border around the timer
+
+// 4. - each time the timer counts down update border
+
+// 5. - when timer reaches 0
+
+// 5.1 reset border
+
+// 5.2 reset timer
---
+++ Day 9 :10 :11/Timer App/TimerClass.js
@@ -0,0 +1,67 @@
+//TIMER CLASS THAT CAN BE REUSED FOR OTHER PROJECTS
+class Timer {
+	constructor(durInp, startBtn, stopBtn, resetBtn, callBacks) {
+		this.durInp = durInp;
+		this.startBtn = startBtn;
+		this.stopBtn = stopBtn;
+		this.resetBtn = resetBtn;
+		if (callBacks) {
+			this.onStart = callBacks.onStart;
+			this.onTick = callBacks.onTick;
+			this.onCompleted = callBacks.onCompleted;
+		}
+
+		//1.2.-event listener for the click buttons
+		this.startBtn.addEventListener("click", this.start);
+		this.startBtn.addEventListener("click", this.start);
+		this.stopBtn.addEventListener("click", this.stop);
+		this.resetBtn.addEventListener("click", this.reset);
+	}
+	//need to define the methods with arrow function so the This keyword is always pointing to the same object
+	//or bind the this keyword in the event
+	//2.0 - Start Method
+	start = () => {
+		if (this.onStart) {
+			this.onStart(this.timeRemaining);
+		}
+
+		this.thick(); //this will start as soon as start is clicked
+		//creating an instance variable with the this word
+		this.timeVal = setInterval(this.thick, 120);
+	};
+	// 3.0-Stop Method
+	stop = () => {
+		console.log("stopped");
+		clearInterval(this.timeVal);
+	};
+	//4.0- Reset Method
+	reset = () => {
+		console.log("Reset");
+	};
+
+	//5.0-Start countdown
+	thick = () => {
+		//when time is completed or equal to 0
+		if (this.timeRemaining <= 0) {
+			this.stop();
+			this.onCompleted();
+		} else {
+			//if time remaining greater than 0
+			if (this.onTick) {
+				this.onTick(this.timeRemaining);
+			}
+			//setter               //getter
+			this.timeRemaining = this.timeRemaining - 0.1;
+		}
+	};
+
+	//this method will allow us to whatever value is passed in the input on the html to be passed into this method
+	// get value
+	get timeRemaining() {
+		return parseFloat(this.durInp.value);
+	}
+	//set value
+	set timeRemaining(time) {
+		this.durInp.value = time.toFixed(1);
+	}
+}
